1398c09e7682b3a9308b54081671b70f3a1caf1c
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-12-01 22:39:41 -0500

    added slider outlet

--- UIKit_Practice/ViewController.swift
+++ UIKit_Practice/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var currentValue: Int = 50
+@IBOutlet weak var slider: UISlider!
+    
+    var currentValue: Int = 0
+    
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currentValue = lroundf(slider.value)
     }
 
     @IBAction func showAlert() {
